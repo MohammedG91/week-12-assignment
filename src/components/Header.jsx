68318b5c61bb68f2e0fdc50506ad3d3accb8f9f6
SitaCicirko <ns.cicirko@gmail.com>
2025-03-03 12:59:49 +0000

    working home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,23 +9,36 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between w-full bg-blue-300 h-10 items-center rounded-md p-1">
-      <h1 className="bg-slate-300 rounded-md border-2">
-        <Link href="/">homepage</Link>
-      </h1>
-      <h1 className="bg-slate-300 rounded-md border-2">
-        <Link href="/profile">profile</Link>
-      </h1>
-      <h1>logo</h1>
-      <aside>
+    <header className="flex justify-between items-center w-full bg-[#4C585B] p-6 shadow-lg">
+      <div className="flex items-center space-x-6">
+        <h1 className="text-2xl font-bold text-[#F4EDD3]">
+          {/* Maybe swap for logo*/}
+          <Link href="/">Local Skills Hub</Link>
+        </h1>
+        <nav className="flex space-x-6">
+          <h1 className="text-lg text-[#F4EDD3] hover:text-[#508c9b] transition duration-300">
+            <Link href="/profile">Profile</Link>
+          </h1>
+          <h1 className="text-lg text-[#F4EDD3] hover:text-[#508c9b] transition duration-300">
+            <Link href="/events">Events</Link>
+          </h1>
+        </nav>
+      </div>
+
+      {/* Sign in/ Sign out */}
+      <aside className="flex items-center space-x-4">
         <SignedOut>
-          <div className="flex gap-2">
-            <SignInButton className="bg-slate-300 rounded-md border-2" />
-            <SignUpButton className="bg-slate-300 rounded-md border-2" />
+          <div className="flex space-x-4">
+            <SignInButton className="bg-[#134b70] text-white px-6 py-3 rounded-lg hover:bg-[#508c9b] transition duration-300 cursor-pointer">
+              Sign In
+            </SignInButton>
+            <SignUpButton className="bg-[#134b70] text-white px-6 py-3 rounded-lg hover:bg-[#508c9b] transition duration-300 cursor-pointer">
+              Sign Up
+            </SignUpButton>
           </div>
         </SignedOut>
         <SignedIn>
-          <UserButton />
+          <UserButton className="cursor-pointer" />
         </SignedIn>
       </aside>
     </header>
