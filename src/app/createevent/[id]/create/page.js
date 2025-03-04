533d3d4f4987693df80bb44604b9800f280ac39a
Amina Said Ibrahim <amina7585@gmail.com>
2025-03-04 10:52:16 +0000

    created event form

---
+++ src/app/createevent/[id]/create/page.js
@@ -0,0 +1,161 @@
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { db } from "@/utils/dbConnection";
+import profilestyle from "../style.module.css";
+
+export default async function createEvent({ params }) {
+  const user = await params;
+
+  const categories = await db.query(`SELECT * FROM event_categories`);
+
+  async function handleSubmit(formData) {
+    "use server";
+
+    const userid = user.id;
+    const eventname = formData.get("eventname");
+    const description = formData.get("description");
+    const category_id = formData.get("selectedCategoryId");
+    const eventdate = formData.get("eventdate");
+    const eventtime = formData.get("eventtime");
+    const location = formData.get("location");
+    const price = formData.get("price");
+    const maxattendees = formData.get("maxattendees");
+    const imageurl = formData.get("imageurl");
+    const ispublic = formData.get("ispublic") === "on";
+    const createdate = new Date().toISOString();
+
+    await db.query(
+      `INSERT INTO events(userid, eventname, description, category_id, eventdate, eventtime, location, price, maxattendees, imageurl, ispublic, createdate) 
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+      [
+        userid,
+        eventname,
+        description,
+        category_id,
+        eventdate,
+        eventtime,
+        location,
+        price,
+        maxattendees,
+        imageurl,
+        ispublic,
+        createdate,
+      ]
+    );
+
+    // Redirecting to the profile page after saving
+    revalidatePath("/profile");
+    redirect("/profile");
+  }
+
+  return (
+    <>
+      <section
+        className={`flex justify-center flex-col items-center w-full m-6 bg-slate-300 p-6 rounded-3xl`}
+      >
+        <h2 className={`${profilestyle.h2} text-black`}>Create Event</h2>
+        <form
+          action={handleSubmit}
+          className="flex flex-col justify-center items-center border-2 border-solid border-gray-700 w-[70vh] p-3 rounded-lg"
+        >
+          <label htmlFor="eventname">Event Name: </label>
+          <input
+            type="text"
+            name="eventname"
+            id="eventname"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="description">Description: </label>
+          <textarea
+            type="text"
+            name="description"
+            id="description"
+            required
+            className="text-green-500 rounded-2xl h-28 p-4"
+          />
+
+          <label htmlFor="category">Select Category</label>
+          <select
+            id="category"
+            name="selectedCategoryId"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          >
+            <option value="">--Select a Category--</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="eventdate">Event Date: </label>
+          <input
+            type="date"
+            name="eventdate"
+            id="eventdate"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="eventtime">Event Time: </label>
+          <input
+            type="time"
+            name="eventtime"
+            id="eventtime"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="location">Location: </label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="price">Price: </label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="maxattendees">Max Attendees: </label>
+          <input
+            type="number"
+            name="maxattendees"
+            id="maxattendees"
+            className="text-green-500 rounded-2xl h-10 p-4"
+            required
+          />
+
+          <label htmlFor="imageurl">Image URL: </label>
+          <textarea
+            type="url"
+            name="imageurl"
+            id="imageurl"
+            className="text-green-500 rounded-2xl h-28 p-4"
+            required
+          />
+
+          <label htmlFor="ispublic">Is Public: </label>
+          <input
+            type="checkbox"
+            name="ispublic"
+            id="ispublic"
+            className="text-green-500 rounded-2xl"
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </>
+  );
+}
