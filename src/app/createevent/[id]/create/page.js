eb40584f923227541ee734991c54db40a04364df
Amina Said Ibrahim <amina7585@gmail.com>
2025-03-04 11:25:48 +0000

    imported revalidate and redirect toevents form page so now form works well

--- src/app/createevent/[id]/create/page.js
+++ src/app/createevent/[id]/create/page.js
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { db } from "@/utils/dbConnection";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export default async function createEvent({ params }) {
   const user = await params;
